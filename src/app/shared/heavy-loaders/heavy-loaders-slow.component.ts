7aaf60534f185558a64ef070c440d77cf9366993
kgs <simonudemy09@gmail.com>
2024-10-24 09:35:26 +0200

    defer views done

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-  <h1>Heavy Slow</h1>
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      <h1>Heavy Slow</h1>
+
+    </section>
   `,
 })
 export class HeavyLoadersSlowComponent {
+
+
+  @Input({ required: true }) cssClass!: string;
+
+  constructor(){
+    console.log('Heavyy loader component');
+
+
+    const start = Date.now();
+    while( Date.now() - start < 3000){}
+  
+    console.log('loaded');
+
+  }
 
 }
